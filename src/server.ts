4335541bf66b59660edfd51328f0400ceccdc06f
Rodney Lab <codesign@rodneylab.com>
2021-10-08 05:19:03 +0100

    refactor: 🏄🏽‍♂️ refactor server to facilitate resolver testing

--- src/server.ts
+++ src/server.ts
@@ -1,97 +1,10 @@
-import { PrismaClient } from '.prisma/client';
-import fastifySession from '@fastify/session';
-import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import { ApolloServer } from 'apollo-server-fastify';
-import { ApolloServerPlugin } from 'apollo-server-plugin-base';
-import 'dotenv/config';
-import Fastify, { FastifyInstance } from 'fastify';
-import fastifyCookie from 'fastify-cookie';
-// import { Server, IncomingMessage, ServerResponse  } from 'http';
-// import fastifyCors from 'fastify-cors';
-import fastifyPostgres from 'fastify-postgres';
-import fastifyRedis from 'fastify-redis';
-import 'reflect-metadata';
-import { buildSchema } from 'type-graphql';
-import { GalleryResolver } from './resolvers/gallery';
-import HelloResolver from './resolvers/hello';
-import TubeStationResolver from './resolvers/tubeStation';
-import UserResolver from './resolvers/user';
-import { isProduction } from './utilities/utilities';
+import app from './app';
 
-if (!isProduction) {
-  delete process.env.https_proxy;
-  delete process.env.HTTPS_PROXY;
-  delete process.env.http_proxy;
-  delete process.env.HTTP_PROXY;
-  delete process.env._proxy;
-}
+(async () => {
+  const server = await app();
 
-function fastifyAppClosePlugin(app: FastifyInstance): ApolloServerPlugin {
-  return {
-    async serverWillStart() {
-      return {
-        async drainServer() {
-          await app.close();
-        },
-      };
-    },
-  };
-}
-
-const prisma = new PrismaClient();
-
-async function startApolloServer() {
-  const server: FastifyInstance = Fastify();
-
-  // server.register(fastifyCors, {
-  //   origin: process.env.CORS_ORIGIN,
-  //   methods: ['GET', 'POST'],
-  //   credentials: true,
-  // });
-
-  server.register(fastifyPostgres, {
-    connectionString: process.env.DATABASE_URL,
+  server.listen(4000, () => {
+    console.log(`Server ready at
+http://localhost:4000/graphql`);
   });
-
-  server.register(fastifyCookie);
-  server.register(fastifyRedis, { host: '127.0.0.1' });
-  server.register(fastifySession, {
-    secret: process.env.SESSION_SECRET as string,
-    cookie: {
-      secure: isProduction,
-      sameSite: 'Lax',
-      maxAge: 1_800_000,
-      domain: isProduction ? `.${process.env.DOMAIN}` : undefined,
-    },
-  });
-  const { redis } = server;
-
-  // server.register(prismaPlugin);
-  // await context.prisma.gallery.deleteMany({});
-  // await prisma.tubeStation.deleteMany({});
-
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [GalleryResolver, HelloResolver, TubeStationResolver, UserResolver],
-      validate: false,
-    }),
-    plugins: [
-      fastifyAppClosePlugin(server),
-      ApolloServerPluginDrainHttpServer({ httpServer: server.server }),
-    ],
-    context: async ({ request, reply }) => ({
-      request,
-      reply,
-      prisma,
-      app: server,
-      redis,
-    }),
-  });
-  await apolloServer.start();
-  server.register(apolloServer.createHandler());
-  await server.listen(4000);
-  console.log(`Server ready at
-  http://localhost:4000${apolloServer.graphqlPath}`);
-}
-
-startApolloServer();
+})();
---
+++ tests/resolvers/hello.ts
@@ -0,0 +1,43 @@
+import type { FastifyInstance } from 'fastify';
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import build from '../../src/app';
+
+let app: FastifyInstance;
+
+test.before(async () => {
+  app = await build();
+});
+
+test.before.each((meta) => {
+  console.log(meta['__test__']);
+});
+
+test.after(() => app.close());
+
+test('it sends expected response to hello query', async () => {
+  try {
+    const query = `
+    query Query {
+      hello
+    }
+  `;
+
+    const response = await app.inject({
+      method: 'POST',
+      url: '/graphql',
+      headers: { 'content-type': 'application/json' },
+      payload: { query, variables: {} },
+    });
+
+    const json = await response.json();
+    assert.snapshot(JSON.stringify(json), '{"data":{"hello":"Hello everybody!"}}');
+    assert.is(json.data.hello, 'Hello everybody!');
+  } catch (error) {
+    console.error(
+      `Error in test resolvers/hello.ts: it sends expected response to hello query: ${error}`,
+    );
+  }
+});
+
+test.run();
